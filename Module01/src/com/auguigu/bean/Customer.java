fd781b44bdbb679d359779f9add56178d36386b2
chenjl <chenjl@qq.com>
2021-08-04 23:41:13 +0800

    commit one

---
+++ Module01/src/com/auguigu/bean/Customer.java
@@ -0,0 +1,14 @@
+package com.auguigu.bean;
+
+
+//djfdkfjdf
+public class Customer {
+    public static void main(String[] args){
+      System.out.println("hello world 2222!!");
+
+    }
+
+    public void method2(){
+        System.out.println("method2");
+    }
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.atguigu.java;
+
+import com.auguigu.bean.Customer;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world !!");
+
+        ArrayList list=new ArrayList();
+        for (int i = 0; i < list.size(); i++) {
+            
+        }
+    }
+
+    private static final Customer CC=new Customer();
+}
